7276db2017e3e7bf96027a326fb79297924354e3
Minimaxi28 <minimaxi@outlook.com>
2026-02-09 22:05:09 +0100

    Draconic NBT fix (but better this time)
    Draconic NBT fix (but better this time)

--- kubejs/server_scripts/recipe_change/08_misc.js
+++ kubejs/server_scripts/recipe_change/08_misc.js
@@ -670,7 +670,7 @@ ServerEvents.recipes((event) => {
   // Draconic Evolution misc recipes
   create3x3(event, 'draconicevolution:grinder', [
     'draconicevolution:draconium_ingot',
-    'draconicevolution:draconic_sword',
+    'kubejs:draconic_sword',
     'draconicevolution:draconium_ingot',
     'mythicbotany:alfsteel_sword',
     'draconicevolution:draconic_energy_core',
